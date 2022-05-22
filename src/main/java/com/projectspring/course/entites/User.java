c1e75d2a6ecc8393ba6df21aeab7f104a03317a8
Cleonildo Jr <cleu.junior@gmail.com>
2022-05-22 01:09:16 -0300

    H2 database, test profile and JPA

--- src/main/java/com/projectspring/course/entites/User.java
+++ src/main/java/com/projectspring/course/entites/User.java
@@ -1,11 +1,20 @@
 package com.projectspring.course.entites;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
 import java.io.Serializable;
 import java.util.Objects;
 
+@Entity
+@Table(name = "tb_user")
 public class User implements Serializable {
     private static final long serialVersionUID = 1L;
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String name;
